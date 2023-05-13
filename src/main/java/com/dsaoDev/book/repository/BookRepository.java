eddf4937ceba47624fa60ea1db31cbf519ea0c31
dsaoDev <davissgt1@hotmail.com>
2023-05-13 00:37:10 -0300

    removed @Repository just testting

--- src/main/java/com/dsaoDev/book/repository/BookRepository.java
+++ src/main/java/com/dsaoDev/book/repository/BookRepository.java
@@ -1,12 +1,10 @@
 package com.dsaoDev.book.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.dsaoDev.book.entity.Book;
 
 //not necessary
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
